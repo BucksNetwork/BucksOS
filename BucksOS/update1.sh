191875c07bfd7307c369144f8bc1d6402585e9c6
R e <122604720+BucksNetwork@users.noreply.github.com>
2023-01-13 09:59:56 +0000

    0.1.1 beta

--- BucksOS/update1.sh
+++ BucksOS/update1.sh
@@ -1,10 +1,1 @@
-# Copy over modes from old version
-  OCTAL_MODE=$(stat -c '%a' $SELF)
-  if ! chmod $OCTAL_MODE "$0.tmp" ; then
-    echo "Failed: Error while trying to set mode on $0.tmp."
-    exit 1
-  fi
-cat > test.sh << EOF
-#!/bin/bash
-test
-EOF 
+git clone https://github.com/BucksNetwork/BucksOS.git
