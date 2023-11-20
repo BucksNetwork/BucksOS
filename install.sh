9f3bf8c0f07d920aca7c4d8c9b6bba8dbba3c0ac
BucksNetwork <122604720+BucksNetwork@users.noreply.github.com>
2023-11-20 12:03:14 +0100

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,7 @@
 mv bucksautostartup.sh ~/etc
 sudo apt-get install htop -y
+echo "$n > "
+read inp
 echo "You need to restart BucksOS to install the update."
 echo "Do you want to start BucksOS now and install the update when you exit BucksOS? y/n"
 if [[( $inp == "y")]]; then 
