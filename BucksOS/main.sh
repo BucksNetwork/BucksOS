ced5e6a28c483d0774eef4247797120dbf7074ed
BucksNetwork <122604720+BucksNetwork@users.noreply.github.com>
2023-11-22 11:09:32 +0100

    Update main.sh

--- BucksOS/main.sh
+++ BucksOS/main.sh
@@ -1,11 +1,13 @@
+clear
 echo "Name: "
-read n 
+read n
+clear
 echo "Password: "
 read p
 if [[( $n == "root" && $p == "0000")]]; then 
+bash$ sed -i 'logged=true' var.txt
 bash ~/BucksOS/BucksOS/run.sh
 exit
 else 
 echo "Wrong password or username..."
 fi
-
